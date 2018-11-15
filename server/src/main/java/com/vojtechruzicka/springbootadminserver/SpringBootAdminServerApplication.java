796cbda65325407e451235a4faf44b0b27ebcc7b
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-11-15 18:18:27 +0100

    Basic server setup

--- server/src/main/java/com/vojtechruzicka/springbootadminserver/SpringBootAdminServerApplication.java
+++ server/src/main/java/com/vojtechruzicka/springbootadminserver/SpringBootAdminServerApplication.java
@@ -1,9 +1,11 @@
 package com.vojtechruzicka.springbootadminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringBootAdminServerApplication {
 
     public static void main(String[] args) {
