d983801fb992e234c1983ae0542bb5f45b3de707
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2018-11-22 19:01:56 +0100

    Testing v 1.x

--- server/src/main/java/com/vojtechruzicka/springbootadminserver/SpringBootAdminServerApplication.java
+++ server/src/main/java/com/vojtechruzicka/springbootadminserver/SpringBootAdminServerApplication.java
@@ -1,6 +1,6 @@
 package com.vojtechruzicka.springbootadminserver;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
