c632d8a5de2ef6e74edcb3bd785467bdfd97f7a0
Vojtech Ruzicka <vojtech.ruz@gmail.com>
2019-01-13 16:22:09 +0100

    Properly downgraded to 1.x

--- server/src/main/java/com/vojtechruzicka/springbootadminserver/SecurityConfig.java
+++ server/src/main/java/com/vojtechruzicka/springbootadminserver/SecurityConfig.java
@@ -4,31 +4,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        SavedRequestAwareAuthenticationSuccessHandler successHandler
-                = new SavedRequestAwareAuthenticationSuccessHandler();
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         successHandler.setTargetUrlParameter("redirectTo");
         successHandler.setDefaultTargetUrl("/");
 
+        http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+        http.logout().logoutUrl("/logout");
+        // Login page and static assets should not be protected
         http.authorizeRequests()
-                .antMatchers("/assets/**").permitAll()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login").successHandler(successHandler).and()
-                .logout().logoutUrl("/logout").and()
-                .httpBasic().and()
-                .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringAntMatchers(
-                        "/instances",
-                        "/actuator/**"
-                );
+            .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+            .permitAll();
+        // Everything else requires authentication
+        http.authorizeRequests()
+            .antMatchers("/**")
+            .authenticated();
+        http.httpBasic();
+        // 1.x does not support csrf
+        http.csrf().disable();
     }
 }
